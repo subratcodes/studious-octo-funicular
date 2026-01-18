ff940283043e1f6e7ee45c7eba708bdc008adf9d
Singh, Subrat (FDNY) <>
2026-01-18 13:54:03 -0500

    Add SerectsManager service and update application properties for native profile

---
+++ config/src/main/java/subrat/learning/config/SerectsManager.java
@@ -0,0 +1,26 @@
+package subrat.learning.config;
+
+import org.springframework.stereotype.Service;
+
+import jakarta.annotation.PostConstruct;
+
+@Service
+public class SerectsManager {
+    
+
+    @PostConstruct
+    public void getTestSecret(){
+
+        
+        
+
+
+
+    }
+
+
+
+
+
+
+}
